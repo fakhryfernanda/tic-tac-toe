095da24762cede7a4066810f3203cb47e81d302a
fakhryfernanda <mff023@gmail.com>
2022-05-11 17:46:57 +0700

    Adding 2 players to the game. Now can write the answers each player gives

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ const gameBoard = document.querySelector('[data-gameBoard]');
 const tiles = gameBoard.querySelectorAll('[data-tile]');
 const clearButton = document.querySelector('.clear-button');
 
-let count = 0;
+// Human always go first
+let human = new Player('Human');
+let computer = new Player('Computer');
+
+let count = 1;
 tiles.forEach(tile => {
     tile.addEventListener('click', (e) => {
-        if (tile.getAttribute('data-tile') === '') {
+        if (tile.getAttribute('data-filled') === 'false') {
             if (count % 2 === 1) {
                 tile.innerHTML = '<i class="fa-solid fa-xmark">';
             } else {
                 tile.innerHTML = '<i class="fa-solid fa-circle">';
             };
     
-            tile.setAttribute('data-tile', 'filled');
+            tile.setAttribute('data-filled', 'true');
+            writeAnswer(tile);
             count += 1;
         };
     });
@@ -22,7 +27,21 @@ clearButton.addEventListener('click', () => {
     resetTiles();
 });
 
-// FUNTIONS 
+// FUNCTIONS
+
+function Player(name) {
+    this.name = name;
+    this.answer = [];
+};
+
+function writeAnswer(tile) {
+    if (count % 2 === 1) {
+        human.answer.push(tile.getAttribute('data-tile'));
+    } else {
+        computer.answer.push(tile.getAttribute('data-tile'));
+    };
+    
+};
 
 function resetTiles() {
     tiles.forEach(tile => {
