9c58289afb2e623c53cbe81eb5be84ae1c4e7957
fakhryfernanda <mff023@gmail.com>
2022-05-13 23:16:42 +0700

    Now can play against computer with random moves. Sometimes game crashed when computer cannot find valid move.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const board = (() => {
     const self = ticTacToe.querySelector('[data-game-board]');
     const prompts = ticTacToe.querySelectorAll('[choose-turn]');
     const tiles = ticTacToe.querySelectorAll('[data-tile]');
+    let filledTiles;
     const startButton = ticTacToe.querySelector('[data-button="start"]');
     const restartButton = ticTacToe.querySelector('[data-button="restart"]');
     const gameInfo = ticTacToe.querySelector('[data-modal="game-info"]');
@@ -35,7 +36,7 @@ const board = (() => {
         element.classList.remove('hide');
     };
 
-    return {self, prompts, tiles, startButton, restartButton, gameInfo, chooseTurn, resetTiles, modalText, hideElement, showElement};
+    return {self, prompts, tiles, filledTiles, startButton, restartButton, gameInfo, chooseTurn, resetTiles, modalText, hideElement, showElement};
 })();
 
 const game = (() => {
@@ -46,7 +47,8 @@ const game = (() => {
     };
 
     const start = function() {
-        human  = new Player('Human', 'x');
+        board.filledTiles = [];
+        human  = new Player('You', 'x');
         computer = new Player('Computer', 'o');
         count = 0;
     
@@ -57,7 +59,34 @@ const game = (() => {
         board.showElement(board.restartButton);         // show restart button
     };
 
+    const playTurn = function() {
+        if (ticTacToe.getAttribute('game-start') === 'true') {
+            if (game.nowTurn === 'human') {
+                board.tiles.forEach(tile => {
+                    tile.addEventListener('click', (e) => {
+                        // tile hanya dapat diisi jika masih kosong
+                        if (tile.getAttribute('data-filled') === 'false') {
+                            human.play(tile);
+                            game.nowTurn = 'computer';
+                        };
+                    });
+                });
+            } else if (game.nowTurn === 'computer') {
+                computer.play();
+                game.nowTurn = 'human';
+            };
+
+            game.resultCheck();  // memeriksa hasil akhir game
+            gameTimeout();
+        };
+    };
+
+    const gameTimeout = function() {
+        setTimeout(playTurn, 1500);
+    };
+
     const stop = function() {
+        clearTimeout(gameTimeout);                      // mencegah computer jalan setelah game selesai
         ticTacToe.setAttribute('game-start', 'false');  // stop the game
         board.self.style.filter = 'blur(5px)';          // blur the game board
         board.showElement(board.gameInfo);              // show the gameInfo
@@ -88,30 +117,49 @@ const game = (() => {
         });
     
         if (winner !== undefined) {
-            board.modalText(board.gameInfo.firstElementChild,`${winner.name} wins!`);
-            game.stop();;
+            board.modalText(board.gameInfo.firstElementChild,`${winner.name} win!`);
+            game.stop();
         } else if (winner === undefined && count === 9) {
             board.modalText(board.gameInfo.firstElementChild, 'Draw!')
             game.stop();
         };
     };
 
-    return {chooseTurn, start, stop, resultCheck};
+    return {chooseTurn, start, playTurn, stop, resultCheck};
 })();
 
 function Player(name, mark) {
     this.name = name;
     this.answer = [];
+    this.goFirst = false;
     this.winner = false;
 
     if (mark === 'x' || mark === 'X') {this.mark = '<i class="fa-solid fa-xmark">'}
     else if (mark === 'o' || mark === 'O') {this.mark = '<i class="fa-solid fa-circle">'};
 
+    const computerThinks = function() {
+        const choices = '123456789'.split('').filter(n => !board.filledTiles.includes(n));
+        console.log(choices);
+        const index = Math.floor(Math.random() * choices.length);
+        decision = ticTacToe.querySelector('[data-tile="' + choices[index] + '"]');
+
+        return decision;
+    };
+
     this.play = function(tile) {
-        this.answer.push(tile.getAttribute('data-tile'));
-        tile.innerHTML = this.mark;
-        count += 1;
-        tile.setAttribute('data-filled', 'true'); // menandai tile sudah diisi
+        // memastikan computer tidak bermain ketika tidak ada pilhan tile
+        if (board.filledTiles.length < 9) {
+            if (name === 'Computer') {
+                tile = computerThinks();
+            }
+    
+            console.log(tile);
+            this.answer.push(tile.getAttribute('data-tile'));
+            tile.innerHTML = this.mark;
+            board.filledTiles.push(tile.getAttribute('data-tile'));
+            count += 1;
+            tile.setAttribute('data-filled', 'true'); // menandai tile sudah diisi
+        };
     };
 };
 
@@ -126,29 +174,41 @@ board.prompts.forEach(prompt => {
         game.start();
 
         if (e.target.getAttribute('choose-turn') === 'human') {
+            game.nowTurn = 'human';
+            human.goFirst = true;
             game.firstTurn = human;
             game.secondTurn = computer;
         } else {
+            game.nowTurn = 'computer';
+            computer.goFirst = true;
             game.firstTurn = computer;
             game.secondTurn = human;
         };
+
+        game.playTurn();
     });
 });
 
-board.tiles.forEach(tile => {
-    tile.addEventListener('click', (e) => {
-        // tile hanya dapat diisi ketika game sudah dimulai
-        if (ticTacToe.getAttribute('game-start') === 'true') {
-            // tile hanya dapat diisi jika masih kosong
-            if (tile.getAttribute('data-filled') === 'false') {
-                if (count % 2 === 0) {
-                    game.firstTurn.play(tile);
-                } else {
-                    game.secondTurn.play(tile);
-                };
+// while(ticTacToe.getAttribute('game-start') === 'true') {
+//     game.playTurn();
+// }
+
+// board.tiles.forEach(tile => {
+//     tile.addEventListener('click', (e) => {
+//         // tile hanya dapat diisi ketika game sudah dimulai
+//         if (ticTacToe.getAttribute('game-start') === 'true') {
+//             // tile hanya dapat diisi jika masih kosong
+//             if (tile.getAttribute('data-filled') === 'false') {
+//                 if (count % 2 === 0) {
+//                     game.firstTurn.play(tile);
+//                 } else {
+//                     game.secondTurn.play(tile);
+//                 };
                 
-                game.resultCheck();  // memeriksa hasil akhir game
-            };
-        };
-    });
-});
+//                 board.filledTiles.push(tile.getAttribute('data-tile'));
+//                 // console.log(board.filledTiles);
+//                 game.resultCheck();  // memeriksa hasil akhir game
+//             };
+//         };
+//     });
+// });
